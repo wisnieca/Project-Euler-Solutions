fd77f1d29cab25c9248352812d8ae1bf593a5556
Cody Wisniewski <wisnieca@mail.uc.edu>
2015-12-11 20:07:42 -0500

    got main styles set up and finished first problem

--- ProjectEulerSolutions/Solutions.js
+++ ProjectEulerSolutions/Solutions.js
@@ -1,1 +1,24 @@
-// JavaScript source code
+var Solutions = {};
+
+Solutions.Initialize = function () {
+    console.log("Problem 001 = " + Solutions.Problem001());
+};
+
+Solutions.Problem001 = function () {
+    // Multiple of 3 and 5
+    /*
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     */
+    var total = 0;
+    for (var i = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) total += i;
+    }
+    return total;
+    // return 233168
+};
+
+$(function () {
+    "use strict";
+    Solutions.Initialize();
+});
